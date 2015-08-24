840a73162d29844bc1d1ce73de86d903f9966e0a
br-monteiro <bruno.monteirodg@gmail.com>
2015-08-23 21:05:33 -0300

    Exercício 3.8 - não resolvido

---
+++ Estrutura-de-Dados/Exercicio/Exercicios_001/03_rep_8_soma_valor_x_a_z_valores.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Escreva um programa em C para ler um valor X e um valor Z (se Z for menor
+ * que X deve ser lido um novo valor para Z). Contar quantos números inteiros
+ * devemos somar em seqüência (a partir do X inclusive) para que a soma
+ * ultrapasse a Z o mínimo possível. Escrever o valor final da contagem.
+ * Exemplo:
+ * X  Z   Reposta
+ * 3  20  5 (3+4+5+6+7=25)
+ * 2  10  4 (2+3+4+5=14)
+ * 30 40 2 (30+31=61)
+ */
+int main() {
+    return 0;
+}
+
