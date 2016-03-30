e615c5d08708b9a453c8b1f8853d5d34bd9129fb
br-monteiro <bruno.monteirodg@gmail.com>
2016-03-30 15:17:14 -0300

    Interface usada para exercícios

---
+++ Linguagem-de-Programacao/Exercicio/ListaDeExercicio02/src/listadeexercicio02/Exercicio.java
@@ -0,0 +1,10 @@
+
+package listadeexercicio02;
+
+/**
+ * @author Bruno Monteiro <bruno.monteirodg@gmail.com>
+ */
+public interface Exercicio {
+    // metodo que executa o exercício escolhido
+    public void start();
+}
