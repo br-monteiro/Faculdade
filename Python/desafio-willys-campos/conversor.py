609491fbbca29b6f41dd3fe14967fbc330c96d26
Bruno Monteiro <bruno.monteirodg@gmail.com>
2017-03-10 11:09:47 -0300

    Add conversão automática de numero para str
    Refatoramento do código para conversão do número para string caso o mesmo não seja do tipo string

--- Python/desafio-willys-campos/conversor.py
+++ Python/desafio-willys-campos/conversor.py
@@ -1,6 +1,10 @@
-﻿# Desenvolvido por Bruno Monteiro <bruno.monteirodg@gmail.com>
+# Desenvolvido por Bruno Monteiro <bruno.monteirodg@gmail.com>
 # LAUS DEO
 def bin_to_dec(numero, soma = 0, index = 0, exp = 0):
+    #converte o valor de numero para str
+    if (type(numero) is not str):
+        numero = str(numero)
+
     # condição de parada. Retorna o resultado
     if index == len(numero) or numero == 0:
         return soma
@@ -14,4 +18,4 @@ def bin_to_dec(numero, soma = 0, index = 0, exp = 0):
     #inicia a brincadeira da recursividade ... =)
     return bin_to_dec(numero, soma + (int(numero[index]) * (2 ** exp)), index + 1, exp - 1)
 
-print('O número binário 1010 é igual a %d em decimal' %(bin_to_dec('1010')))
+print('O número binário 1010 é igual a %d em decimal' %(bin_to_dec(1010)))
